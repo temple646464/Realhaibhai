6bd82d6f7033edef01f95673a5d78a7797ac1094
temple646464 <Temple646464@gmail.com>
2025-04-28 17:19:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 api_id = "28748671"
 api_hash = "f53ec7c41ce34e6d585674ed9ce6167c"
 bot_token = "8086953920:AAGYBOwM4ysQ0bZKrOJn0IADQE439nz1t2E"
-auth_users = [1169394017]
+auth_users = [1169394017,7294890574]
